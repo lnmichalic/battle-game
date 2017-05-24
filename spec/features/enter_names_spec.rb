f6b3d77624cb5444e74ded1e352f4177ea91fbb5
Mário Ribeiro <mail@marioribeiro.com>
2017-05-24 16:15:39 +0100

    Add each player's hit points (HP)
    - Add CSS file for styling

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,4 +1,5 @@
 require './app'
+
 feature 'add players' do
   scenario 'allows you to add players' do
     visit '/'
@@ -6,5 +7,15 @@ feature 'add players' do
     fill_in('player2', with: 'Rob')
     click_button('Add players')
     expect(page).to have_content('John vs Rob')
+  end
+end
+
+feature "check opponent's HP" do
+  scenario 'player can check points' do
+    visit '/'
+    fill_in('player1', with: 'John')
+    fill_in('player2', with: 'Rob')
+    click_button('Add players')
+    expect(page).to have_content('60 / 60 HP')
   end
 end
