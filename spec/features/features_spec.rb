8c2bb423ed0e610c4df1dc580b7aa04b7c10d307
jade <jadealvares@hotmail.co.uk>
2017-05-24 15:24:58 +0100

    rewritten test to take 2 players and make them vs. each other

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -1,9 +1,10 @@
 require './app'
-feature 'add_players' do
-  it 'allows you to add players' do
+feature 'add players' do
+  scenario 'allows you to add players' do
     visit '/'
-    fill_in('name', with: 'John')
-    click_button('Add player')
-    expect(page).to have_content('John')
+    fill_in('player1', with: 'John')
+    fill_in('player2', with: 'Rob')
+    click_button('Add players')
+    expect(page).to have_content('John vs Rob')
   end
 end
